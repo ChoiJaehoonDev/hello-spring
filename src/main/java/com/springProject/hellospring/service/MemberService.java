ff7619113f08628aa7d34133ef9742042db01a24
JaeHoon <52474292+fofo0623@users.noreply.github.com>
2022-01-09 16:25:38 +0900

    회원 서비스 테스트

--- src/main/java/com/springProject/hellospring/service/MemberService.java
+++ src/main/java/com/springProject/hellospring/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.springProject.hellospring.service;
 
 import com.springProject.hellospring.domain.Member;
+import com.springProject.hellospring.repository.MemberRepository;
 import com.springProject.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
@@ -8,32 +9,35 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemoryMemberRepository MemberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
 
     //회원 가입
     public Long join(Member member){
- validateDuplicateMember(member); //중복회원 검증
+        validateDuplicateMember(member); //중복회원 검증
 
-
-        MemberRepository.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
         //동명이인은 안된다
-        //null 가능성 -> Optional으로 감싸기
-        MemberRepository.findByName(member.getName()).ifPresent(m->{;
+
+        memberRepository.findByName(member.getName()).ifPresent(m->{;
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
     }
 
     public List<Member> findMembers(){
-        return MemberRepository.findAll();
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId){
-        return MemberRepository.findById(memberId);
+        return memberRepository.findById(memberId);
     }
 
 }
