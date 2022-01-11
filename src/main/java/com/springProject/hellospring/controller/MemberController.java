2abb3f00f909f23ab78142927af28d04503fe121
JaeHoon <52474292+fofo0623@users.noreply.github.com>
2022-01-11 13:40:24 +0900

    회원 웹 기능 - 등록
    get방식 / post방식 차이 및 웹 흐름

--- src/main/java/com/springProject/hellospring/controller/MemberController.java
+++ src/main/java/com/springProject/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.springProject.hellospring.controller;
 
+import com.springProject.hellospring.domain.Member;
 import com.springProject.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,20 @@ public class MemberController {
     @Autowired  //spring container에서 memberservice를 가져옴
     public MemberController(MemberService memberService) {      //DI
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")     //get방식 데이터를 조회할 때 주로 사용
+    public String createFrom(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")    //post방식 데이터를 form같은 형식으로 전달할 때 주로 사용
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        System.out.println("member = " + member.getName());
+        memberService.join(member);
+
+        return "redirect:/";    //전 화면으로 돌려보내기
     }
 }
